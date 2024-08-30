17bfd4f3628829f06441da32293264ba5ea90716
COUREL Kylian <llcerespagnoluniversitederouen@hotmail.com>
2024-08-31 01:33:54 +0200

    2ème grande modif (erasmus, espace commentaires)

---
+++ JAVA SCRIPT/plugin.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const userId = {
+    name: null,
+    identity: null,
+    image: null,
+    message: null,
+    date: null
+}
+
+const userComment = (".usercomment");
+const publierBtn = ("#publier")
